2d9c012da8c9fb2ea3aa8cc9152f49a1b926d0e9
Sharon Fraiman <sharon.fraiman@gm.com>
2020-06-06 12:46:51 +0300

    Implementing the TimerSync

--- src/main_mytimer.cpp
+++ src/main_mytimer.cpp
@@ -3,40 +3,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <iostream>
 
-#include "mytimer.h"
+#include "mytimer_OO.h"
 
-void time_handler1(size_t timer_id, void* user_data)
+using namespace std;
+
+void testTimer(void)
 {
-    printf ("Single shot timer expired (%zu)\n", timer_id);
-}
-
-void time_handler2(size_t timer_id, void* user_data)
-{
-    printf ("1000ms periodic timer expired (%zu)\n", timer_id);
-}
-
-void time_handler3(size_t timer_id, void* user_data)
-{
-    printf ("2000ms periodic timer expired (%zu)\n", timer_id);
+    cout << "Creating testTimer" << endl;
+    TimerSync mytimer(4000);    // Synchronous timer with 4 seconds blocking time
+    
+    cout << "Starting testTimer" << endl;
+    mytimer.startTimer();
+    
+    cout << "Exiting testTimer" << endl;
 }
 
 int main()
 {
-    size_t timer1, timer2, timer3;
+    cout << "Starting main" << endl;
+    testTimer();
     
-    initialize();
-    
-    timer1 = start_timer(3000, time_handler1, TIMER_SINGLE_SHOT, NULL);
-    timer2 = start_timer(1000, time_handler2, TIMER_PERIODIC, NULL);
-    timer3 = start_timer(2000, time_handler3, TIMER_PERIODIC, NULL);    
-    
-    sleep(6);
-    
-    stop_timer(timer1);
-    stop_timer(timer2);
-    stop_timer(timer3);
-    
-    finalize();
+    return 0;
 }
+
+
+// #include "mytimer.h"
+
+// void time_handler1(size_t timer_id, void* user_data)
+// {
+    // printf ("Single shot timer expired (%zu)\n", timer_id);
+// }
+
+// void time_handler2(size_t timer_id, void* user_data)
+// {
+    // printf ("1000ms periodic timer expired (%zu)\n", timer_id);
+// }
+
+// void time_handler3(size_t timer_id, void* user_data)
+// {
+    // printf ("2000ms periodic timer expired (%zu)\n", timer_id);
+// }
+
+// int main()
+// {
+    // size_t timer1, timer2, timer3;
+    
+    // initialize();
+    
+    // timer1 = start_timer(3000, time_handler1, TIMER_SINGLE_SHOT, NULL);
+    // timer2 = start_timer(1000, time_handler2, TIMER_PERIODIC, NULL);
+    // timer3 = start_timer(2000, time_handler3, TIMER_PERIODIC, NULL);    
+    
+    // sleep(6);
+    
+    // stop_timer(timer1);
+    // stop_timer(timer2);
+    // stop_timer(timer3);
+    
+    // finalize();
+// }
     
--- src/mytimer_OO.h
+++ src/mytimer_OO.h
@@ -1,6 +1,7 @@
 /* mytimer_OO.h */
 #include <iostream>
 #include <sys/timerfd.h>
+#include <poll.h>
 #include <system_error>
 
 using namespace std;
@@ -8,16 +9,42 @@ using namespace std;
 class TimerBase
 {
 protected:
-    TimerBase(unsigned int initialTime) : m_initialTime(initialTime)
+    TimerBase(unsigned int initialTime)
     {   
         cout << "TimerBase CTOR" << endl;
+
+        /* Translate time from initialTime milliseconds 
+           to struct itimerspec/struct timespec */
+        m_timerSpec.it_value.tv_sec = initialTime / 1000;
+        m_timerSpec.it_value.tv_nsec = (initialTime % 1000) * 1000000;
     }
     
-    virtual void startTimer(void); // shared capability between Sync and ASync
-    // virtual void stopTimer(void); // This is an Async timer capability only
-private:
-    int                m_fd;
-    unsigned int       m_initialTime;   // Initial time of timer. For sync timer, the only timer value
+    void initTimer(void) 
+    {
+        // Create timer file descriptor 
+        m_fd = timerfd_create(CLOCK_REALTIME, /* int clockid */
+                              0);             /* int flags   */
+        if (m_fd == -1)
+        {
+            throw system_error (errno,
+                                system_category(),
+                                "Failed to timerfd_create");
+        }
+
+        // Arm timer and activate it
+        timerfd_settime(m_fd,           /* int fd */
+                        0,              /* int flags */
+                        &m_timerSpec,   /* const struct itimerspec *new_value */
+                        NULL);          /* struct itimerspec *old_value */
+    }
+    
+    virtual void startTimer(void) = 0;
+    virtual void stopTimer(void) = 0; 
+    
+protected:
+    int                 m_fd;
+    struct itimerspec   m_timerSpec;    
+    
     // time_handler       callback;
     // void*               user_data;
     // unsigned int        interval;
@@ -25,37 +52,91 @@ private:
     // struct timer_node*  next;
 };
 
+
+
+
+/* Synchronous Timer - Blocking timer - Equivalent to sleep but with millisecond resolution*/
 class TimerSync : public TimerBase
 {
 public:
-  TimerSync (unsigned int initialTime) : TimerBase(initialTime)
+  TimerSync (unsigned int initialTime) :  TimerBase(initialTime)
   {
-    m_fd = timerfd_create(CLOCK_REALTIME, 0);
-    if (m_fd == -1)
+      cout << "TimerSync CTOR" << endl;      
+      // Synchronous timers are blocking, so cannot use periodic timers with interval
+        m_timerSpec.it_interval.tv_sec = 0;
+        m_timerSpec.it_interval.tv_nsec = 0;  
+  }
+  
+  ~TimerSync(void)
+  {
+    cout << "TimerSync DTOR" << endl;
+    stopTimer();
+  }
+  
+  void startTimer(void)
+  {
+    int read_fd = 0;
+    
+    /* Use a single pollfd array for the synchronous timer*/
+    struct pollfd ufds[1] = {{ .fd = m_fd,
+                               .events = POLLIN,
+                               .revents = 0}};    
+    
+    /* Create timer fd, and initialize settime */
+    initTimer();
+
+    cout << "TimerSync startTimer " << endl; 
+    #define INFINITE_TIMEOUT (-1)
+    /* Block on the file descriptor */
+    read_fd = poll(ufds,                 /* struct   pollfd *fds */
+                   1,                    /* nfds_t   nfds */
+                   INFINITE_TIMEOUT);    /* int timeout : Specifying a negative value in  timeout means  an infinite timeout.*/
+                   // 6000);    /* int timeout : Specifying a negative value in  timeout means  an infinite timeout.*/                   
+                            
+
+    /* Error checking for poll system call */
+    if (read_fd <= 0)
     {
-        throw system_Error (errno,
-                            system_category(),
-                            "Failed to timerfd_create");
+        // Either an error has occurred (read_fs < 0)
+        // or timed out, without any event on the file descriptors (read_fd == 0)
+        cout << "TimerSync : Error in read_fd : " << read_fd << endl;
+    }
+    
+    /* Check if event POLLIN was raised */
+    if (ufds[0].revents & POLLIN)
+    {
+        cout << "TimerSync : Returned with POLLIN " << endl;
+    }
+    else
+    {
+        cout << "TimerSync : Returned without POLLIN " << endl;
     }
   }
 
+  void stopTimer(void)
+  {
+      close(m_fd);
+  }
+    
 };
 
+// -------------------------------------------------------------------------
 
+/* Asynchronous Timer - Non Blocking Timer - Relies on a timer thread to carry out the callback */
 class TimerASync : public TimerBase
 {
 public:
-  TimerSync (unsigned int waitTime)
+  TimerASync (unsigned int initialTime,
+             unsigned int intervalTime) : TimerBase(initialTime)
   {
-    m_fd = timerfd_create(CLOCK_REALTIME, 0);
-    if (m_fd == -1)
-    {
-        throw system_Error (errno,
-                            system_category(),
-                            "Failed to timerfd_create");
-    }
+    cout << "TimerASync CTOR" << endl;      
+    m_timerSpec.it_interval.tv_sec = intervalTime / 1000;
+    m_timerSpec.it_interval.tv_nsec = (intervalTime % 1000) * 1000000;  
   }
 
-private:
-    unsigned int m_intervalTime;
+    // void startTimer(void)
+    // {
+        
+    // }
+    // void stopTimer(void); 
 };
